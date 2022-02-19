f1ded460e35a2240819e008f93ca657f92396d61
Trinh <chekhanhtrinh@gmail.com>
2022-02-19 17:00:57 -0500

    while and for loops in C#

---
+++ Lesson4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Lesson4
+{
+    class Program
+    {
+            //keep asking for pasword
+        static void Main(string[] args)
+        {
+            string correctPassword = "trinniedabest";
+            string userPassword;
+
+            do
+            {
+                Console.WriteLine("Please enter your password:");
+                userPassword = Console.ReadLine();
+            }
+            while (correctPassword != userPassword);
+
+            Console.WriteLine("Access granted.");
+        }
+    }
+}
---
+++ Lesson5/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Lesson5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // user inputs first and end number using loop //
+            Console.WriteLine("Enter first number:");
+            string userInput1 = Console.ReadLine();
+            double userNumberFirst = Convert.ToDouble(userInput1);
+            Console.WriteLine("Enter end number:");
+            string userInput2 = Console.ReadLine();
+            double userNumberEnd = Convert.ToDouble(userInput2);
+            while (userNumberEnd <= userNumberFirst)
+            {
+                Console.WriteLine("The end number cannot be equal or smaller to first. Please re-enter the end number:");
+                userInput2 = Console.ReadLine();
+                userNumberEnd = Convert.ToDouble(userInput2);
+            }
+
+            double i;
+            Console.WriteLine("Baby, this is what you came for ~~: ");
+            for (i = userNumberFirst;i<=userNumberEnd;i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            //////////// COUNT USING WHILE LOOP ////////////////
+            //string userInput;
+            //double userNumber;
+            //int i = 0;
+            //Console.WriteLine("Enter a whole, positive number:");
+            //userInput = Console.ReadLine();
+            //userNumber = Convert.ToDouble(userInput);
+            //while (userNumber < 0)
+            //{
+            //    Console.WriteLine("I said WHOLE, POSITIVE. Re-enter now: ");
+            //    userInput = Console.ReadLine();
+            //    userNumber = Convert.ToDouble(userInput);
+
+            //}
+            //Console.WriteLine("The series of numbers is as follows: ");
+            //while (i < userNumber)
+            //{
+            //    i ++;
+            //    Console.WriteLine(i);
+            //}
+
+
+
+
+
+            ////////// SIMPLE WHILE LOOP /////////////
+            //int i = 0;
+
+            //int userInput = 10;
+
+            //while (i < userInput)
+            //{
+            //    i = i + 1;
+            //    Console.WriteLine(i);
+            //}
+        }
+    }
+}
