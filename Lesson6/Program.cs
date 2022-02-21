50ad0a3c9d721a91222a9ec6cf1de0f9d07fb1e0
Trinh <chekhanhtrinh@gmail.com>
2022-02-20 22:49:01 -0500

    Learned how to create geometrical objects

---
+++ Lesson6/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Lesson6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        //////// DRAW A TRIANGLE ////////
+            Console.Write("Enter a number:");
+            int whatever = int.Parse(Console.ReadLine());
+            for(int i = whatever; i==0; i--)
+            {
+                Console.Write("#");
+            }
+
+
+
+
+
+
+
+
+
+        ////////// DRAW A RECTANGLE ///////
+
+        //Console.Write("Please enter length:");
+        //int length = int.Parse(Console.ReadLine());
+        //Console.Write("Please enter width:");
+        //int width = int.Parse(Console.ReadLine());
+
+        //for (int j = 0; j < width ; j++)
+        //{
+        //    Console.WriteLine();
+        //    for (int x = 0; x < length; x++)
+        //    {
+        //        Console.Write("#");
+        //    }
+        //}
+
+    }
+    }
+}
