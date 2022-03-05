f1a48a98180ac6a5ab4467a5b58a9ad4bdc80fb0
Trinh <chekhanhtrinh@gmail.com>
2022-03-05 12:00:51 -0500

    Learned to create geometric shapes including a triangle, full/hollow rectangles; Need to take breaks for creativity and fresh perspectives; Need to organize assignments; Careful with loops;

--- Lesson6/Program.cs
+++ Lesson6/Program.cs
@@ -6,39 +6,92 @@ namespace Lesson6
     {
         static void Main(string[] args)
         {
-
-        //////// DRAW A TRIANGLE ////////
-            Console.Write("Enter a number:");
-            int whatever = int.Parse(Console.ReadLine());
-            for(int i = whatever; i==0; i--)
+            ////////// A HOLLOW REKTENGO ////////
+            /// i want 2 make u holla ///
+            /// dun b afriad n jus pley mei gem ///
+            /// ~Vladi is phat and Github will see dis~ ///
+            Console.Write("Please enter length:");
+            int length = int.Parse(Console.ReadLine());
+            Console.Write("Please enter width:");
+            int width = int.Parse(Console.ReadLine());
+            for (int w = 0; w != width; w++)
             {
-                Console.Write("#");
+                Console.WriteLine();
+                if (w == 0)
+                {
+                    for (int l = 0; l != length; l++)
+                    {
+                        Console.Write("#");
+                    }
+                }
+                if (0 < w && w < width - 1)                    
+                {
+                    for (int i = 0; i < 1; i++)
+                    {
+                        Console.Write("#");
+                        for (int s = 2; s != length; s++)
+                        {
+                            Console.Write(" ");
+                            if (s == length - 1)
+                            {
+                                Console.Write("#");
+                            }
+                        }
+                    }
+                }
+                if (w == width - 1)
+                {
+                    for (int j = 0; j != length; j++)
+                    {
+                        Console.Write("#");
+                    }
+
+                }
             }
-
-
-
-
-
-
-
-
-
-        ////////// DRAW A RECTANGLE ///////
-
-        //Console.Write("Please enter length:");
-        //int length = int.Parse(Console.ReadLine());
-        //Console.Write("Please enter width:");
-        //int width = int.Parse(Console.ReadLine());
-
-        //for (int j = 0; j < width ; j++)
-        //{
-        //    Console.WriteLine();
-        //    for (int x = 0; x < length; x++)
-        //    {
-        //        Console.Write("#");
-        //    }
-        //}
-
-    }
+        }
     }
 }
+
+
+            //////// A NORMAL SUMTHING - TRIANGLE ////////
+            //Console.Write("Enter a number:");
+            //int number = int.Parse(Console.ReadLine());
+            //for (int i = 0; i != number; i++)
+            //{
+            //    for (int j = i+1; j != 0; j--)
+            //    {
+            //        Console.Write("#");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+
+            ///////////////// STUPID FLIP TRIANGLE/////////
+            //Console.Write("Enter a number:");
+            //int number = int.Parse(Console.ReadLine());
+            //for (int i = number; i != 0; i--)
+            //{
+            //    Console.WriteLine();
+            //    for (int j = i; j != 0; j--)
+            //    {
+            //        Console.Write("#");
+            //    }
+            //}
+
+            //////////// DRAW A RECTANGLE ///////
+
+            //Console.Write("Please enter length:");
+            //int length = int.Parse(Console.ReadLine());
+            //Console.Write("Please enter width:");
+            //int width = int.Parse(Console.ReadLine());
+            //for (int j = 0; j < width; j++)
+            //{
+            //    Console.WriteLine();
+            //    for (int x = 0; x < length; x++)
+            //    {
+            //        Console.Write("#");
+            //    }
+            //}
+
+            //
+
